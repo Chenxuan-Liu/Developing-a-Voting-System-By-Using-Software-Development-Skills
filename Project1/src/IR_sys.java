edabc5f94562a32bbd74dc0105da93ee66eb80d1
Jicheng Zhu <zhu00224@umn.edu>
2021-03-09 22:42:44 -0600

    tweaks

--- Project1/src/IR_sys.java
+++ Project1/src/IR_sys.java
@@ -17,8 +17,8 @@ public class IR_sys extends Voting_Sys{
 		} else if (candidates.size() == 1){
 			return candidates.get(0);
 		} else if (candidates.size() == 2){
-			Integer vote0 = candidates.get(0).getVote();
-			Integer vote1 = candidates.get(1).getVote();
+			int vote0 = candidates.get(0).getVote();
+			int vote1 = candidates.get(1).getVote();
 			if (vote0 > vote1){
 				return candidates.get(0);
 			} else if (vote1 > vote0){
@@ -28,7 +28,7 @@ public class IR_sys extends Voting_Sys{
 			}
 		}
 		
-		for(Integer i = 0; i < candidates.size(); i ++){
+		for(int i = 0; i < candidates.size(); i ++){
 			int vote = candidates.get(i).getVote();
 			if(vote > totalballot/2){
 				return candidates.get(i);
@@ -37,17 +37,17 @@ public class IR_sys extends Voting_Sys{
 		return null;
 	}
 	
-	public Integer get_leastcandidate(){
-		Integer least_vote = candidates.get(0).getVote();
-		Integer least_candidate = 0;
+	public int get_leastcandidate(){
+		int least_vote = candidates.get(0).getVote();
+		int least_candidate = 0;
 		
-		for(Integer i = 0; i < candidates.size(); i ++){
-			Integer vote = candidates.get(i).getVote();
+		for(int i = 0; i < candidates.size(); i ++){
+			int vote = candidates.get(i).getVote();
 			if(vote < least_vote){
 				least_candidate = i;
 				least_vote = vote;
 			} else if(vote == least_vote){
-				Integer[] random = {i,least_candidate};
+				int[] random = {i,least_candidate};
 				least_candidate = random[coinflip(2)];
 				least_vote = candidates.get(least_candidate).getVote();
 			}
@@ -56,13 +56,13 @@ public class IR_sys extends Voting_Sys{
 	}
 	
 	public void redistribution(){
-		Integer least = get_leastcandidate();
+		int least = get_leastcandidate();
 		Candidate candidate = candidates.get(least);
 		ArrayList<Ballot> ballots = candidate.getBallots();
 		Ballot ballot;
 		
 		//ballots redistribution
-		for(Integer i = 0; i < ballots.size(); i++){
+		for(int i = 0; i < ballots.size(); i++){
 			ballot = ballots.get(i);
 			ballot.updateRank();
 			if (ballot.getRank() < ballot.getRanksize()){
