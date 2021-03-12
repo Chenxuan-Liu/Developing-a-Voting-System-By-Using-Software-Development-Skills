fa57b11995fb611ce624066c4e9b24a7c319725c
Chenxuan Liu <liu00464@umn.edu>
2021-03-11 21:36:33 -0600

    modify unit test

--- Project1/src/Coin_FlipTest.java
+++ Project1/src/Coin_FlipTest.java
@@ -1,4 +1,11 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
+
 public class Coin_FlipTest {
-  
+
+    @Test
+    public void flip() {
+
+    }
 }
--- Project1/src/PartyTest.java
+++ Project1/src/PartyTest.java
@@ -1,4 +1,41 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
 import static org.junit.Assert.*;
+
 public class PartyTest {
-  
+    private ArrayList<Candidate> can = new ArrayList<>();
+    private Party p1 = new Party(0,"p1", can);
+
+//    @Before
+//    public void setUp() {
+//
+//    }
+
+    @Test
+    public void getVote() {
+        assertEquals(0, p1.getVote());
+    }
+
+    @Test
+    public void setVote() {
+        p1.setVote(100);
+        assertEquals(100, p1.getVote());
+        p1.setVote(-1);
+        assertEquals(-1, p1.getVote());
+    }
+
+    @Test
+    public void getName() {
+        assertEquals("p1", p1.getName());
+    }
+
+    @Test
+    public void getMembers() {
+        assertEquals(null, p1.getMembers());
+        can.add(new Candidate("c1", p1));
+        assertEquals(can, p1.getMembers());
+    }
 }
